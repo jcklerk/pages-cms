962f71db73dae8624513c1329823774a30a92051
Justin Klerk <justin@klerk.nl>
2025-01-16 14:48:12 +0100

    Update collaborator.ts

--- lib/actions/collaborator.ts
+++ lib/actions/collaborator.ts
@@ -62,7 +62,7 @@ const handleAddCollaborator = async (prevState: any, formData: FormData) => {
 
 		Promise.resolve().then(async () => {
       const { data, error } = await resend.emails.send({
-				from: "Pages CMS <no-reply@mail.pagescms.org>",
+				from: "Pages CMS <no-reply@pages-cms.jcklerk.dev>",
 				to: [email],
 				subject: `Join "${owner}/${repo}" on Pages CMS`,
 				react: InviteEmailTemplate({
